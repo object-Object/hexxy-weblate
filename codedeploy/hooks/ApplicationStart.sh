2aa05a1c64f4d47adbcc957eab305ec4065a219d
object-Object <object@objectobject.ca>
2026-08-01 23:44:36 -0400

    Increase ApplicationStart timeout

--- codedeploy/hooks/ApplicationStart.sh
+++ codedeploy/hooks/ApplicationStart.sh
@@ -3,7 +3,7 @@ set -euox pipefail
 
 cd /var/lib/codedeploy-apps/hexxy-weblate
 
-if ! docker compose up --detach --wait --wait-timeout 120 ; then
+if ! docker compose up --detach --wait --wait-timeout 480 ; then
     docker compose logs
     exit 1
 fi
